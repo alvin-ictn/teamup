d4790c28b9a4e29ca1d59bb8e32e43865c8fd736
Alvin Mantovani <alvin.ictn@gmail.com>
2020-10-01 23:06:46 +0700

    add october project 1

---
+++ Projects/October 2020/JavaScript Challange/001.js
@@ -0,0 +1,30 @@
+
+/**
+Buatlah sebuah script untuk menampilkan nama & nilai dari siswa di kelas :
+Seorang pengajar sedang memeriksa ujian mahasiswa dan akan memberikan desc nilai dari A-E dengan ketentuan sebagai berikut:
+ - Nilai 80 - 100: A
+ - Nilai 65 - 79: B
+ - Nilai 50 - 64: C
+ - Nilai 35 - 49: D
+ - Nilai 0 - 34: E
+
+ Tampilkan desc nilai dan nama siswa saat pengajar tersebut memasukkan nilai dan nama yang dia inginkan.
+
+NOTED:
+Jika nilai mahasiswa kurang dari 0 atau lebih dari 100 maka tampilkan 'Nilai Invalid'
+contoh : 
+1.
+name = 'Rocky'
+nilai = '90'
+output : 'Nilai Rocky mendapatkan nilai A'
+
+name = 'John Doe'
+nilai = '0'
+output : 'Nilai Invalid
+
+**/ 
+
+const nilai = (nama,nilai) => {
+
+}
+console.log(nilai("A",20))
---
+++ Projects/October 2020/JavaScript Challange/002 .js
@@ -0,0 +1,25 @@
+/**
+---------------------
+First Letter Grouping
+---------------------
+
+Diberikan sebuah variabel `name`. Buatlah sebuah script untuk menampilkan nama tersebut
+berada di group ke-berapa, dengan aturan sebagai berikut:
+- Jika huruf pertama dari `name` adalah a, b, c, d maka tampilkan 'Masuk group pertama'
+- Jika huruf pertama dari `name` adalah e, f, g, h maka tampilkan 'Masuk group kedua'
+- Selain itu tampilkan 'Masuk group terakhir'
+
+Contoh:
+- `name` = 'daniel', output: 'Masuk group pertama'
+- `name` = 'immelda', output: 'Masuk group kedua'
+- `name` = 'taufik', output: 'Masuk group terakhir'
+- `name` = 'sergei', output: 'Masuk group terakhir'
+
+*/
+
+const grup = (nama) => {
+}
+
+console.log(grup("daniel"))
+console.log(grup("himelda"))
+console.log(grup("taufik"))
